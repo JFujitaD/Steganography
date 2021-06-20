791977e8dbfc856b72a2b8b24a0ec604134cada4
JFujitaD <fujitajulian@icloud.com>
2021-06-20 14:18:40 -0600

    Added another test image

--- src/Steganography.java
+++ src/Steganography.java
@@ -2,7 +2,7 @@
 public class Steganography {
 
 	public static void main(String[] args) {
-		ImageManager im = new ImageManager("src/Images/3x3.png");
+		ImageManager im = new ImageManager("src/Images/computer.png");
 		im.printImageHeader();
 		im.printImageInformation();
 		im.printChunks();
